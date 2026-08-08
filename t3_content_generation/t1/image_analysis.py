8afe793cf424d48534ec081cf7214ce85f425a57
Narek Hovhannisyan <narek_hovhannisyan1@epam.com>
2026-08-08 12:22:53 +0400

    Implement image analysis

--- t3_content_generation/t1/image_analysis.py
+++ t3_content_generation/t1/image_analysis.py
@@ -1,20 +1,45 @@
 import base64
+from pathlib import Path
 
-from commons.constants import OPENAI_HOST
+from commons.constants import GPT_5_4_NANO, OPENAI_HOST
 from t3_content_generation._openai_client import OpenAIClientT3
 
 
-# https://developers.openai.com/api/docs/guides/images-vision?format=url&lang=curl
-# https://developers.openai.com/api/docs/guides/images-vision?format=base64-encoded
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
 
-#TODO:
-# You need to analyse these 2 images:
-#   - https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg
-#   - in this folder we have 'logo.png', load it as encoded data (see documentation)
-# ---
-# Hints:
-#   - Use OpenAIClientT3 to connect to OpenAI API
-#   - Use /v1/chat/completions endpoint
-#   - Function to encode image to base64 you can find in documentation
-# ---
-# In the end load both images (url and base64 encoded 'logo.png'), ask "Generate poem based on images" and se what will happen?
+
+parent_dir = Path(__file__).resolve().parent
+dial_logo_path = parent_dir / "logo.png"
+base64_dial_logo = encode_image(dial_logo_path)
+
+print(f"base64_dial_logo : {base64_dial_logo}")
+
+input_messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "Generate poem based on images."},
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": "https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg"
+                },
+            },
+            {
+                "type": "image_url",
+                "image_url": {"url": f"data:image/png;base64,{base64_dial_logo}"},
+            },
+        ],
+    }
+]
+
+OpenAIClientT3(f"{OPENAI_HOST}/v1/chat/completions").call(
+    print_request=True,
+    print_response=True,
+    # model="gpt-realtime-2.1-mini",
+    model=GPT_5_4_NANO,
+    messages=input_messages,
+    max_completion_tokens=1024,
+)
