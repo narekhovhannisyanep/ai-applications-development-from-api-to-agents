f69b69d7dd7754bbcf461e99a74c96c22e4c9ff3
Narek Hovhannisyan <narek_hovhannisyan1@epam.com>
2026-08-08 14:16:29 +0400

    Implement image generation

--- t3_content_generation/t2/gpt_image_generation.py
+++ t3_content_generation/t2/gpt_image_generation.py
@@ -1,9 +1,8 @@
 import base64
-from datetime import datetime
+import pathlib
 
 from commons.constants import OPENAI_HOST
 from t3_content_generation._openai_client import OpenAIClientT3
-
 
 # https://developers.openai.com/api/reference/resources/images/methods/generate
 # ---
@@ -25,7 +24,8 @@ from t3_content_generation._openai_client import OpenAIClientT3
 #   ]
 # }
 
-#TODO:
+
+# TODO:
 # You need to create some images with `gpt-image-2` model:
 #   - Generate an image with 'Smiling catdog'
 #   - Decode and save it locally
@@ -34,3 +34,20 @@ from t3_content_generation._openai_client import OpenAIClientT3
 #   - Use OpenAIClientT3 to connect to OpenAI API
 #   - Use /v1/images/generations endpoint
 #   - The image will be returned in base64 format
+
+image_response = OpenAIClientT3(f"{OPENAI_HOST}/v1/images/generations").call(
+    print_request=True,
+    print_response=True,
+    model="gpt-image-1-mini",
+    prompt="smiling catdog",
+    n=1,
+    size="1024x1024",
+    quality="low",
+)
+
+image_bytes = base64.b64decode(image_response.get("data", [])[0].get("b64_json"))
+
+catdog_image_file = pathlib.Path(__file__).resolve().parent / "catdog.png"
+
+with open(catdog_image_file, "wb") as f:
+    f.write(image_bytes)
