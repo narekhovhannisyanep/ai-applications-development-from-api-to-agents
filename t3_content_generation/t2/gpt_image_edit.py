5fe494f1fd889141197e7b6c4cac48b74a9c89a0
Narek Hovhannisyan <narek_hovhannisyan1@epam.com>
2026-08-09 13:27:16 +0400

    Implement speech to speech

--- t3_content_generation/t2/gpt_image_edit.py
+++ t3_content_generation/t2/gpt_image_edit.py
@@ -1,6 +1,5 @@
 import base64
 import pathlib
-from datetime import datetime
 
 import requests
 
--- t3_content_generation/t5/speech_to_speech.py
+++ t3_content_generation/t5/speech_to_speech.py
@@ -1,15 +1,16 @@
 import base64
-import json
-from datetime import datetime
+import pathlib
 
 import requests
 
-from commons.constants import OPENAI_API_KEY, OPENAI_HOST
-
+from commons.constants import (
+    OPENAI_API_KEY,
+    OPENAI_CHAT_COMPLETIONS_ENDPOINT,
+)
 
 # https://developers.openai.com/api/docs/guides/audio#add-audio-to-your-existing-application
 
-#TODO:
+# TODO:
 # You need to generate answer in audio format based on the audio message:
 #   - Create Client that is similar with OpenAIClients but extracts from message audio (instead of content)
 #   - Call API
@@ -23,3 +24,42 @@ from commons.constants import OPENAI_API_KEY, OPENAI_HOST
 #   - Use similar method to encode audio as you have done for images encoding
 
 
+current_dir = pathlib.Path(__file__).resolve().parent
+question_file_name = current_dir / "question.mp3"
+headers = {"Authorization": f"Bearer {OPENAI_API_KEY}"}
+
+with open(question_file_name, "rb") as question_file:
+    encoded_question = base64.b64encode(question_file.read()).decode("utf-8")
+    json_payload = {
+        "model": "gpt-audio-mini",
+        "modalities": ["text", "audio"],
+        "audio": {"voice": "nova", "format": "mp3"},
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "What is in the recording?"},
+                    {
+                        "type": "input_audio",
+                        "input_audio": {"format": "mp3", "data": encoded_question},
+                    },
+                ],
+            }
+        ],
+    }
+
+    response = requests.post(
+        url=OPENAI_CHAT_COMPLETIONS_ENDPOINT, headers=headers, json=json_payload
+    )
+
+if response.status_code != 200:
+    raise requests.exceptions.HTTPError(f"HTTP {response.status_code} {response.text}")
+
+data = response.json()
+audio_content = (
+    data.get("choices", [])[0].get("message", {}).get("audio", {}).get("data")
+)
+print(data)
+print()
+with open("audio_output.mp3", "wb") as audio_output_file:
+    audio_output_file.write(base64.b64decode(audio_content))
