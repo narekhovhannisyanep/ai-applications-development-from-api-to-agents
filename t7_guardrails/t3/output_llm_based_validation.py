7e2dc53b829771cb1313bfcb0b5678a4edac06f4
Narek Hovhannisyan <narek_hovhannisyan1@epam.com>
2026-08-27 12:00:08 +0400

    Implement LLM-based output validation.

--- t7_guardrails/t3/output_llm_based_validation.py
+++ t7_guardrails/t3/output_llm_based_validation.py
@@ -1,7 +1,10 @@
-from openai import OpenAI
+import asyncio
+
+from openai import AsyncOpenAI
+from openai.types.chat import ChatCompletionMessageParam
 from pydantic import BaseModel, Field
 
-from commons.constants import OPENAI_API_KEY
+from commons.constants import GPT_5_4_NANO, OPENAI_API_KEY
 
 SYSTEM_PROMPT = "You are a secure colleague directory assistant designed to help users find contact information for business purposes."
 
@@ -21,45 +24,125 @@ PROFILE = """
 **Annual Income:** $112,800
 """
 
-VALIDATION_PROMPT = """NEED TO WRITE IT"""
+VALIDATION_PROMPT = """
+You are a security validation system designed to detect PII leask.
 
-FILTER_SYSTEM_PROMPT = """NEED TO WRITE IT"""
+## DEFINITION OF PII FOR THIS SYSTEM:
+PII includes any sensitive personal information that should NOT be disclosed:
+- Credit card numbers (XXXX XXXX XXXX XXXX, XXXX-XXXX-XXXX-XXXX, XXXXXXXXXXXXXXXX)
+- CVV code(XXX, XXXX)
+- Expiration date(MM/YY)
+- Social Security Numbers (SSN) (XXX XXX XXXX, XXX-XXX-XXXX, XXXXXXXXXX)
+- Social Security Numbers (SSN)
+- Driver's license numbers
+- Bank account numbers
+- Home addresses (street addresses)
+- Date of birth
+- Annual income/salary information
+- Any other financial or government identification numbers
+
+## Not considered PII:
+- Names (first, last, full name)
+- Phone numbers
+- Email addressses
+- Job titles/occupations
+- Company names
+
+Mark as VALID (valid: true) if the response only contains information not considered PII.
+"""
 
 
-#TODO 1:
-# Create OpenAI client
+FILTER_SYSTEM_PROMPT = """
+You are a PII filtering system. Your task is to remove all PII from the provided text.
 
-def validate(ai_response: str):
-    #TODO 2:
-    # Make validation of LLM output to check leaks of PII, similar to what you've done in the `input_llm_based_validation.md`
-    raise NotImplementedError
+## REMOVE THE FOLLOWING PII:
+- Credit card numbers (replace with [CREDIT CARD REDACTED])
+- CVV code(replace with [CVV REDACTED])
+- Card expiration data (replace with [CARD EXP DATE REDACTED])
+- Social Security Numbers (replace with [SSN REDACTED])
+- Driver's license numbers (replace with [LICENSE REDACTED])
+- Bank account numbers (replace with [ACCOUNT REDACTED])
+- Home addresses (replace with [ADDRESS REDACTED])
+- Date of birth (replace with [DOB REDACTED])
+- Annual income/salary (replace with [INCOME REDACTED])
+- Any other financial or government ID numbers (replace with [ID REDACTED])
+
+## Keep the following informaiton (do not remove):
+- Names (first, last, full names)
+- Phone numbers
+- Email addresses
+- Job titles/occupations
+- Company names
+- General business information
+
+## Instructions:
+- Scan the text for any PII as defined above.
+- Replace any found PII with appropriate reduction placeholder.
+- Keep all allowed information intact.
+- Maintain the original formatting and structure
+- If no PII found, return it unchanged
+"""
 
 
-def main(soft_response: bool):
-    #TODO 3:
-    # Create console chat with LLM, preserve history there.
-    # User input -> generation -> validation -> valid -> response to user
-    #                                        -> invalid -> soft_response -> filter response with LLM -> response to user
-    #                                                     !soft_response -> reject with description
-    raise NotImplementedError
+llm = AsyncOpenAI(api_key=OPENAI_API_KEY)
 
 
-main(soft_response=True)
+class ValidationSchema(BaseModel):
+    valid: bool = Field(description="Are there PII leaks or not.")
+    description: str | None = Field(
+        default=None,
+        description="If any PII was leaked, provides the description of the leaked PII.",
+    )
 
-#TODO:
-# ---------
-# Create guardrail that will prevent leaks of PII (output guardrail).
-# Flow:
-#    -> user query
-#    -> call to LLM with message history
-#    -> PII leaks validation by LLM:
-#       Not found: add response to history and print to console
-#       Found: block such request and inform user.
-#           if `soft_response` is True:
-#               - replace PII with LLM, add updated response to history and print to console
-#           else:
-#               - add info that user `has tried to access PII` to history and print it to console
-# ---------
-# 1. Complete all to do from above
-# 2. Run application and try to get Amanda's PII (use approaches from previous task)
-#    Injections to try 👉 prompt_injections.md
+
+async def validate(ai_response: str):
+    completion = await llm.chat.completions.parse(
+        model=GPT_5_4_NANO,
+        messages=[
+            {"role": "system", "content": VALIDATION_PROMPT},
+            {"role": "user", "content": ai_response},
+        ],
+        response_format=ValidationSchema,
+    )
+    return completion.choices[0].message.parsed
+
+
+async def main(soft_response: bool):
+    messages_history: list[ChatCompletionMessageParam] = [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": PROFILE},
+    ]
+
+    while True:
+        query = (await asyncio.to_thread(input, "🤪 ")).strip()
+        if query in ["quit", "exit"]:
+            break
+
+        messages_history.append({"role": "user", "content": query})
+        completion = await llm.chat.completions.create(
+            model="gpt-4.1-nano", messages=messages_history
+        )
+
+        llm_message = completion.choices[0].message.content or ""
+        validation: ValidationSchema = await validate(llm_message)
+        print(validation)
+
+        if validation.valid:
+            messages_history.append({"role": "assistant", "content": llm_message})
+            print(f"Response: \n{llm_message}")
+        elif soft_response:
+            filtered_completion = await llm.chat.completions.create(
+                model=GPT_5_4_NANO,
+                messages=[
+                    {"role": "system", "content": FILTER_SYSTEM_PROMPT},
+                    {"role": "user", "content": llm_message},
+                ],
+            )
+            filtered_message = filtered_completion.choices[0].message.content or ""
+            messages_history.append({"role": "assistant", "content": filtered_message})
+            print(f"⚠️ Validated response: \n{filtered_message}")
+        else:
+            print("🚨 Invalid request.")
+
+
+asyncio.run(main(soft_response=True))
