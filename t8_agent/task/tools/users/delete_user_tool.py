45c794fe4312120db3e620c2e8f918e5a46d663d
Narek Hovhannisyan <narek_hovhannisyan1@epam.com>
2026-08-28 14:59:32 +0400

    Implement tools

--- t8_agent/task/tools/users/delete_user_tool.py
+++ t8_agent/task/tools/users/delete_user_tool.py
@@ -2,27 +2,33 @@ from typing import Any
 
 from t8_agent.task.tools.users.base import BaseUserServiceTool
 
-class DeleteUserTool(BaseUserServiceTool):
 
+class DeleteUserTool(BaseUserServiceTool):
     @property
     def name(self) -> str:
-        #TODO: Provide tool name as `delete_users`
-        raise NotImplementedError()
+        return "delete_users"
 
     @property
     def description(self) -> str:
-        #TODO: Provide description of this tool
-        raise NotImplementedError()
+        return "Delete user by ID."
 
     @property
     def input_schema(self) -> dict[str, Any]:
-        #TODO:
-        # Provide tool params Schema. This tool applies user `id` (number) as a parameter and it is required
-        raise NotImplementedError()
+        return {
+            "type": "object",
+            "properties": {
+                "id": {"type": "number", "description": "ID of user to delete."}
+            },
+            "required": ["id"],
+        }
 
     def execute(self, arguments: dict[str, Any]) -> str:
-        #TODO:
+        # TODO:
         # 1. Get int `id` from arguments
         # 2. Call user_client delete_user and return its results
         # 3. Optional: You can wrap it with `try-except` and return error as string `f"Error while deleting user by id: {str(e)}"`
-        raise NotImplementedError()
+        try:
+            id = arguments["id"]
+            return self._user_client.delete_user(int(id))
+        except Exception as e:
+            return f"Error while deleting {id} user: {e}"
--- t8_agent/task/tools/users/search_users_tool.py
+++ t8_agent/task/tools/users/search_users_tool.py
@@ -1,33 +1,38 @@
+import inspect
 from typing import Any
 
 from t8_agent.task.tools.users.base import BaseUserServiceTool
 
 
 class SearchUsersTool(BaseUserServiceTool):
-
     @property
     def name(self) -> str:
-        #TODO: Provide tool name as `search_users`
-        raise NotImplementedError()
+        return "search_users"
 
     @property
     def description(self) -> str:
-        #TODO: Provide description of this tool
-        raise NotImplementedError()
+        # TODO: Provide description of this tool
+        return inspect.cleandoc("""
+            Search user by various criteria.
+            Name, surname and email support partial matching(case-sensitive).
+            Gender must be an exact match.
+        """)
 
     @property
     def input_schema(self) -> dict[str, Any]:
-        #TODO:
-        # Provide tool params Schema:
-        # - name: str
-        # - surname: str
-        # - email: str
-        # - gender: str
-        # None of them are required (see UserClient.search_users method)
-        raise NotImplementedError()
+        return {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string", "description": "User name"},
+                "surnae": {"type": "string", "description": "User surname"},
+                "email": {"type": "string", "description": "User email"},
+                "gender": {"enum": ["male", "female", "other"]},
+            },
+            "required": [],
+        }
 
     def execute(self, arguments: dict[str, Any]) -> str:
-        #TODO:
-        # 1. Call user_client search_users (with `**arguments`) and return its results
-        # 2. Optional: You can wrap it with `try-except` and return error as string `f"Error while searching users: {str(e)}"`
-        raise NotImplementedError()
+        try:
+            return self._user_client.search_users(**arguments)
+        except Exception as e:
+            return f"Error while searching users: {e}"
